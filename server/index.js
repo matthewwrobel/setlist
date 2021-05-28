960173c86e583e7631bc79ec4ca075dda0976b96
Matthew Wrobel <matthew.wrobel@gmail.com>
2021-05-28 14:26:15 -0700

    saved changes

--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ app.post('/songs', (req, res) => {
 
   getSpotifyLink(song.title, song.composer)
     .then((result) => {
+      // console.log(result);
       let url = result.data.tracks.items[0].external_urls.spotify;
       song.url = url;
     })
